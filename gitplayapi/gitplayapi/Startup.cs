d397ef58b35144d897d080951e9a74e6d3961239
Sergii Diachenko <sergii.diachenko@quipu.de>
2022-02-02 16:47:07 +0200

    4

--- gitplayapi/gitplayapi/Startup.cs
+++ gitplayapi/gitplayapi/Startup.cs
@@ -30,6 +30,7 @@ namespace gitplayapi
             // 1
             // 2
             // 3
+            // 4
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
